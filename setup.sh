bbf328a7b9961bc7eedc49561018ca6186dd9042
gabimarti <1541061+gabimarti@users.noreply.github.com>
2021-03-15 09:10:44 +0100

    Creacion setup.sh y stats.py, no finalizados

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Configuración de display oled
+
+clear
+
+# Aviso
+echo Asegurate de activar primero la comunicación I2C
+echo después reinicia el equipo 
+sudo raspi-config
+
+# Descarga de librerias y dependencias
+
